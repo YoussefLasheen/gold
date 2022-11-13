60574f9a4ececa0322a02d543e62859dcaa1d5a1
Youssef Lasheen <youssef@lasheen.dev>
2022-11-13 12:12:01 +0200

    Add shareprfs to kick in when internet is not availble

--- lib/api.dart
+++ lib/api.dart
@@ -1,16 +1,28 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:gold/rates.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<RatesDerivatives> fetchRates() async {
-  final response = await http
-      .get(Uri.parse('https://www.freeforexapi.com/api/live?pairs=USDXAU,USDEGP'));
+  final prefs = await SharedPreferences.getInstance();
 
-  if (response.statusCode == 200) {
-    Rates rates = Rates.fromJson(jsonDecode(response.body));
-    return RatesDerivatives.deriv(rates);
-  } else {
-    throw Exception('Failed to load album');
+  try {
+    final response = await http.get(
+        Uri.parse('https://www.freeforexapi.com/api/live?pairs=USDXAU,USDEGP'));
+    if (response.statusCode == 200) {
+      await prefs.setString('data', response.body);
+      Rates rates = Rates.fromJson(jsonDecode(response.body));
+      return RatesDerivatives.deriv(rates);
+    }
+  } on SocketException catch (_) {
+    final String? data = prefs.getString('data');
+
+    if (data != null) {
+      Rates rates = Rates.fromJson(jsonDecode(data));
+      return RatesDerivatives.deriv(rates);
+    }
   }
+  throw Exception('Failed to load album');
 }
