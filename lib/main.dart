b03e05130d9a2c9fd645bd74a742fe621f6c7b0e
Youssef Lasheen <youssef@lasheen.dev>
2022-11-22 00:19:03 +0200

    Add translation

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:gold/constants.dart';
 import 'package:gold/main_screen.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
