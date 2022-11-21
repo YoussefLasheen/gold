04e03a3c732755ecf390e2b4b38a0bd66467e184
Youssef Lasheen <youssef@lasheen.dev>
2022-11-22 00:48:21 +0200

    Reforamat to optimize rebuilding

---
+++ lib/components/price_chips/price_chips.dart
@@ -0,0 +1,101 @@
+import 'package:awesome_icons/awesome_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:gold/api.dart';
+import 'package:gold/rates.dart';
+
+import 'package:timeago/timeago.dart' as timeago;
+
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'widgets/carat_pricing.dart';
+import 'widgets/price_card.dart';
+
+
+class PriceChips extends StatefulWidget {
+  final String currencyCode;
+
+  const PriceChips({super.key, required this.currencyCode});
+
+  @override
+  State<PriceChips> createState() => _PriceChipsState();
+}
+
+class _PriceChipsState extends State<PriceChips> {
+  late Future<RatesDerivatives> futureAlbum;
+
+  @override
+  void didUpdateWidget(covariant PriceChips oldWidget) {
+    futureAlbum = fetchRates(widget.currencyCode);
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchRates(widget.currencyCode);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    timeago.setLocaleMessages(
+        'ar', timeago.ArMessages()); // Add french messages
+    return Column(
+      children: [
+        FutureBuilder<RatesDerivatives>(
+          future: futureAlbum,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasData) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  CaratPricing(
+                    data: snapshot.data!,
+                    currencyCode: widget.currencyCode,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: PriceCard(
+                        title: AppLocalizations.of(context)!.ouncePrice,
+                        price: snapshot.data!.XAUUSD,
+                        icon: const Icon(
+                          FontAwesomeIcons.coins,
+                          color: Color(0xFFCCA653),
+                        ),
+                        currencyCode: widget.currencyCode,
+                      )),
+                      Expanded(
+                          child: PriceCard(
+                        title: AppLocalizations.of(context)!.dollarPrice,
+                        price: snapshot.data!.rates.USDXXX,
+                        icon: const Icon(FontAwesomeIcons.dollarSign,
+                            color: Colors.green),
+                        currencyCode: widget.currencyCode,
+                      )),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    '${AppLocalizations.of(context)!.lastUpdate} ${timeago.format(snapshot.data!.rates.timestamp, locale: Localizations.localeOf(context).toLanguageTag())}',
+                  ),
+                ],
+              );
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+
+            return const CircularProgressIndicator();
+          },
+        ),
+      ],
+    );
+  }
+}
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,120 +1,57 @@
-import 'package:awesome_icons/awesome_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:gold/api.dart';
-import 'package:gold/rates.dart';
-import 'package:gold/settings_dialog.dart';
-import 'package:intl/intl.dart';
+import 'package:gold/components/chart_card.dart';
+import 'package:gold/components/price_chips/price_chips.dart';
 
-import 'package:timeago/timeago.dart' as timeago;
+import 'settings_dialog.dart';
 
-import 'components/carat_pricing.dart';
-import 'components/chart_card.dart';
-import 'components/price_card.dart';
-
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
-class MainScreen extends StatefulWidget {
+class MainScreen extends StatelessWidget {
   final String currencyCode;
+
   const MainScreen({super.key, required this.currencyCode});
-  @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  late Future<RatesDerivatives> futureAlbum;
-  
-  @override
-  void didUpdateWidget(covariant MainScreen oldWidget) {
-    futureAlbum = fetchRates(widget.currencyCode);
-    super.didUpdateWidget(oldWidget);
-  }
-  @override
-  void initState() {
-    super.initState();
-    futureAlbum = fetchRates(widget.currencyCode);
-  }
 
   @override
-  Widget build(BuildContext context, ) {
-    //final locale = ref.watch(localeProvider);
-    timeago.setLocaleMessages('ar', timeago.ArMessages()); // Add french messages
+  Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton.extended(
-        icon: Icon(Icons.refresh),
-        label: Text(AppLocalizations.of(context)!.refresh),
-        onPressed: () {
-          setState(() {
-            futureAlbum = fetchRates(widget.currencyCode!);
-          });
-        },
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: FutureBuilder<RatesDerivatives>(
-            future: futureAlbum,
-            builder: (context, snapshot) {
-              if(snapshot.connectionState == ConnectionState.waiting){
-                return Center(child: CircularProgressIndicator());
-              }
-              if (snapshot.hasData) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                  child: Column(
-                    children: [
-                      Align(
-                        alignment: AlignmentDirectional.topStart,
-                        child: IconButton(
-                          icon: Icon(Icons.settings),
-                          onPressed: () {
-                            showGeneralDialog(
-                                context: context,
-                                barrierDismissible: true,
-                                barrierLabel: 'Profile',
-                                pageBuilder: (BuildContext buildContext,
-                                    Animation<double> animation,
-                                    Animation<double> secondaryAnimation) {
-                                  return FadeTransition(
-                                    opacity: animation,
-                                    child: SettingsDialog(),
-                                  );
-                                });
-                          },
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 50,
-                      ),
-                      ChartCard(),
-                      SizedBox(
-                        height: 50,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          CaratPricing(data: snapshot.data!, currencyCode: widget.currencyCode,),
-                          SizedBox(height: 10,),
-                          Row(
-                            children: [
-                              Expanded(child: PriceCard(title: AppLocalizations.of(context)!.ouncePrice,price: snapshot.data!.XAUUSD, icon: Icon(FontAwesomeIcons.coins, color: Color(0xFFCCA653),),currencyCode: widget.currencyCode,)),
-                              Expanded(child: PriceCard(title: AppLocalizations.of(context)!.dollarPrice,price: snapshot.data!.rates.USDXXX, icon: Icon(FontAwesomeIcons.dollarSign, color: Colors.green),currencyCode: widget.currencyCode,)),
-                            ],
-                          ),
-                          
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text('${AppLocalizations.of(context)!.lastUpdate} ${timeago.format(snapshot.data!.rates.timestamp, locale: Localizations.localeOf(context).toLanguageTag())}',),
-                        ],
-                      ),
-                    ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                Align(
+                  alignment: AlignmentDirectional.topStart,
+                  child: IconButton(
+                    icon: Icon(Icons.settings),
+                    onPressed: () {
+                      showGeneralDialog(
+                          context: context,
+                          barrierDismissible: true,
+                          barrierLabel: 'Profile',
+                          pageBuilder: (BuildContext buildContext,
+                              Animation<double> animation,
+                              Animation<double> secondaryAnimation) {
+                            return FadeTransition(
+                              opacity: animation,
+                              child: SettingsDialog(),
+                            );
+                          });
+                    },
                   ),
-                );
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
-
-              return const CircularProgressIndicator();
-            },
+                ),
+                const SizedBox(
+                  height: 100,
+                ),
+                const ChartCard(),
+                const SizedBox(
+                  height: 75,
+                ),
+                PriceChips(currencyCode: currencyCode),
+              ],
+            ),
           ),
         ),
       ),
